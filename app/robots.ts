ab13e9b47ba352f02f6f47c6142c2811d23d3ef5
mydigitalcrown <mydigitalcrown@gmail.com>
2026-04-22 13:24:46 +0530

    feat: add comprehensive redirects from old sitemap, update robots, and create llms.txt

--- app/robots.ts
+++ app/robots.ts
@@ -6,10 +6,12 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/_next/'],
+        disallow: ['/api/', '/_next/', '/admin/'],
       },
     ],
     sitemap: 'https://hotsanjana.in/sitemap.xml',
     host: 'https://hotsanjana.in',
   }
 }
+
+// Note: LLMs.txt is served from public/llms.txt at https://hotsanjana.in/llms.txt
